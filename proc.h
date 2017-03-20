b095a54e377517e8b5ef844dde9bebb3e5dd948a
james <tagprofosho2@gmail.com>
2017-03-19 22:55:15 -0600

    Renamed some things. Added an ENUM that represents the state of a task. Changed indenting from 2 spaces to 4.

--- proc.h
+++ proc.h
@@ -1,29 +1,35 @@
 
-struct process* proc_table;
+struct task_struct* task_list;
 
-struct processor_state { 
-  int r0;
-  int r1;
-  int r2;
-  int r3;
-  int r4;
-  int r5;
-  int r6;
-  int r7;
-  int r8;
-  int r9;
-  int r10;
-  int r11;
-  int r12;
-  int r13;
-  int r14;
-  int r15;  
+struct cpu_registers {
+    int r0;
+    int r1;
+    int r2;
+    int r3;
+    int r4;
+    int r5;
+    int r6;
+    int r7;
+    int r8;
+    int r9;
+    int r10;
+    int r11;
+    int r12;
+    int r13;
+    int r14;
+    int r15;
 }__attribute__((packed)) 
 
-struct process{
-  struct processor_state;
-  unsigned int pid;
-  
-  
-  
+enum task_state{
+    initial,    // This is the initial state when a process is first started/created.
+    ready,      // Task is not currently running but is ready to run.
+    running,    // Currently executing.
+    waiting,    // Not running, and not able to run; presumably until some blocking operation completes.
+    zombie      // Task has exited and is waiting to have its return code harvested by it's parent
+};
+
+struct task_struct{
+    struct cpu_registers;
+    unsigned int pid;
+    task_state state;
 }
