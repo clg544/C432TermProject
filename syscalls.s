d35fb8ec3eb04ca3067369c288f30fe5d615d00c
Alita Mann (adm246) <alita.mann@usask.ca>
2017-03-23 14:52:17 -0600

    Syscalls

---
+++ syscalls.s
@@ -0,0 +1,18 @@
+/* Call svc using r7 as syscall id */
+
+.global syscall
+syscall:
+	push {r7}
+	mov r7, #0x0
+	svc 0
+	pop {r7}
+	bx lr
+
+.global fork
+fork:
+	push {r7}
+	mov r7, #0x1
+	svc 0
+	pop {r7}
+	bx lr
+
