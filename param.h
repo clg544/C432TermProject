c709c04864783d82364d831cd45fe5def0240f33
Clinton Galbraith <clg544@mail.usask.ca>
2017-04-08 19:02:34 -0600

    Removed unused definitions from proc.h, param.h

--- param.h
+++ param.h
@@ -1,6 +1,2 @@
 #define TASK_LIMIT 10
 #define STACK_SIZE	256
-
-#define NPROC     64         /* maximum number of processes */
-#define PROCSIZE  0x3000000  /* The size of one process.    */	
-
--- proc.h
+++ proc.h
@@ -6,28 +6,6 @@
 enum procstate { SLEEPING, RUNNING, RUNNABLE, EXITED, UNUSED };
 enum schedulePriority { HIGH, MEDIUM, LOW };
 
-/* Struct that holds a snapshot of the process. */
-struct context { /*{r0,r1,r2,r3,r4,r5,r6,r7,r8,r9,r10,fp,ip,lr}*/
-  int r0;
-  int r1;
-  int r2;
-  int r3;
-  int r4;
-  int r5;
-  int r6;
-  int r7;
-  int r8;
-  int r9;
-  int r10;
-  int r11;
-  int r12;
-  int sp;
-  int lr;
-  int pc;
-  int apsr;
-} __attribute__ ((packed));
-
-
 /* OS structure that records process information. */
 struct process {
   enum procstate state;
